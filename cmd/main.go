9433f0995e50a0d8e9251b4f7032a67fb853d54d
Powdersumm <nikolaymetlayev@gmail.com>
2025-05-11 20:24:51 +0300

    Initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,18 @@
 package main
 
-import "github.com/Powdersumm/Yandexlmsfinalproject/internal/application"
+import (
+	"log"
+
+	"github.com/Powdersumm/Yandexlmsfinalproject/internal/application"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Предупреждение: .env файл не найден, переменные окружения не загружены")
+	}
+}
 
 func main() {
 	app := application.New()
