00676dfeba5bd95318d88f7b46c72ae4580b9340
Powdersumm <nikolaymetlayev@gmail.com>
2025-05-13 00:29:08 +0300

    Initial commit

--- database/database.go
+++ database/database.go
@@ -12,20 +12,31 @@ import (
 
 var DB *gorm.DB
 
-func Connect() error { // Теперь функция возвращает error
-	dsn := os.Getenv("DB_DSN")
+func Connect() error {
+	// Формируем DSN из переменных окружения
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
 	log.Println("Подключение к базе данных:", dsn)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return fmt.Errorf("ошибка подключения к базе данных: %w", err) // Возвращаем ошибку вместо log.Fatal
+		return fmt.Errorf("ошибка подключения: %w", err)
 	}
 
+	// Включаем расширение для UUID и выполняем миграции
+	db.Exec("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\"")
 	if err := db.AutoMigrate(&models.User{}, &models.Expression{}); err != nil {
-		return fmt.Errorf("ошибка миграции базы данных: %w", err)
+		return fmt.Errorf("ошибка миграции: %w", err)
 	}
 
 	DB = db
-	log.Println("Успешное подключение к базе данных!")
-	return nil // Возвращаем nil при успешном подключении
+	log.Println("Успешное подключение!")
+	return nil
 }
